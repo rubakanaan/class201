f774ffb1336b8b0889a8cc3212934de2b3f43301
root <root@STDLTUC-L069.localdomain>
2021-04-14 13:35:28 +0300

    fix the 7th question loop

--- js/app.js
+++ js/app.js
@@ -1,83 +1,68 @@
 'use strict';
-let score=0;
-let userName= prompt('PLEASE INTER YOUR NAME');
-alert('HELLO '+ userName.toUpperCase()+' ,I AM GLAD YOU ARE HERE!');
+let score = 0;
+let userName = prompt('PLEASE INTER YOUR NAME');
+alert('HELLO ' + userName.toUpperCase() + ' ,I AM GLAD YOU ARE HERE!');
 alert('LETS PLAY A QUICK GAME!');
-   
-let study=prompt('Am I a Computer Scientist?','yes or no').toLocaleLowerCase();
-if (study === 'y' || study ==='yes')
-{
-    alert ('Correct! I studied Computer Science in Yarmouk University.');
-    score++;
-}
-else if (study === 'n' || study ==='no')
-{
-    alert('Sorry, Not Correct');
-}
-else 
-{
- alert("your answer should be yes or no.")
-}
 
-let live= prompt('I Live In Jordan ?','yes or no').toLocaleLowerCase();
-if (live === 'y' || live ==='yes')
-{
-    alert ('Correct! I live in Amman.');
+let study = prompt('Am I a Computer Scientist?', 'yes or no').toLocaleLowerCase();
+if (study === 'y' || study === 'yes') {
+    alert('Correct! I studied Computer Science in Yarmouk University.');
     score++;
 }
-else if(live === 'n' || live ==='no')
-{
+else if (study === 'n' || study === 'no') {
     alert('Sorry, Not Correct');
 }
-else
-{
+else {
     alert("your answer should be yes or no.")
 }
 
-let drink=prompt('Do I Like Drinking Coffe?','yes or no').toLocaleLowerCase();
-if (drink === 'y' || drink ==='yes')
-{
-    alert ('Correct! Coffee is my favourite drink.');
+let live = prompt('I Live In Jordan ?', 'yes or no').toLocaleLowerCase();
+if (live === 'y' || live === 'yes') {
+    alert('Correct! I live in Amman.');
     score++;
 }
-else if (drink === 'n' || drink ==='no')
-{
+else if (live === 'n' || live === 'no') {
     alert('Sorry, Not Correct');
 }
-else 
-{
+else {
+    alert("your answer should be yes or no.")
+}
+
+let drink = prompt('Do I Like Drinking Coffe?', 'yes or no').toLocaleLowerCase();
+if (drink === 'y' || drink === 'yes') {
+    alert('Correct! Coffee is my favourite drink.');
+    score++;
+}
+else if (drink === 'n' || drink === 'no') {
+    alert('Sorry, Not Correct');
+}
+else {
     alert("your answer should be yes or no.");
-    
+
 }
 
-let morning=prompt('Am I a Morning Person?','yes or no').toLocaleLowerCase();
+let morning = prompt('Am I a Morning Person?', 'yes or no').toLocaleLowerCase();
 morning.toLocaleLowerCase();
-if (morning === 'n' || morning ==='no')
-{
-    alert ('Correct! I\'m Not a Morning person.');
+if (morning === 'n' || morning === 'no') {
+    alert('Correct! I\'m Not a Morning person.');
     score++;
 }
-else if (morning === 'y' || morning ==='yes')
-{
+else if (morning === 'y' || morning === 'yes') {
     alert('Sorry, Not Correct');
 }
-else
-{
+else {
     alert("your answer should be yes or no.")
 }
 
-let cat=prompt('Am I a Cat Person?','yes or no').toLocaleLowerCase();
-if (cat === 'y' || cat ==='yes')
-{
-    alert ('Correct! I love Cat So Much.');
+let cat = prompt('Am I a Cat Person?', 'yes or no').toLocaleLowerCase();
+if (cat === 'y' || cat === 'yes') {
+    alert('Correct! I love Cat So Much.');
     score++;
 }
-else if (cat === 'n' || cat ==='no')
-{
+else if (cat === 'n' || cat === 'no') {
     alert('Sorry, Not Correct');
 }
-else
-{
+else {
     alert("your answer should be yes or no.")
 }
 
@@ -91,56 +76,58 @@ console.log(' I love Cat So Much.'); */
 
 
 
-let myNum= 7;
-let counter=1;
-let userNum=Number(prompt("Guess my favorite number between 1 to 10"));
+let myNum = 7;
+let counter = 0;
+let userNum = Number(prompt("Guess my favorite number between 1 to 10"));
 
-while(counter!=4 && userNum != myNum)
-{
-    if(userNum<myNum)
-    {
-       userNum=prompt('TOO LOW , TRY AGAIN!');
-   }
-   else 
-   {
-      userNum=prompt('TOO HIGH , TRY AGAIN!');  
-   }
-   counter++;
+while (counter != 4 && userNum != myNum) {
+    if (userNum < myNum) {
+        userNum = prompt('TOO LOW , TRY AGAIN!');
+        counter++;
+    }
+    else {
+        userNum = prompt('TOO HIGH , TRY AGAIN!');
+        counter++;
+    }
+
 
 }
 
-if (counter==4)
-{
+if (counter == 4) {
     alert('The right answer is 7, sorry!');
 }
-else
-{
+else {
     alert('CORRECT :)');
     score++;
 }
 
 //console.log('7');
 
-let answers=[3,8,12,19,15,6];
-let attemp=0;
-let userAttemp=Number(prompt('Please guess numbers between 1 and 20'));
+let answers = [3, 8, 12, 19, 15, 6];
+let attemp = 0;
+let chance=6;
 
-while(attemp<6)
-{
-    for(let i=0; i<answers.length;i++){
-    if (userAttemp==answers[i])
-    {
-         userAttemp=prompt('Correct, Try to guess another number :)');
-         attemp++;
-         score++;
-    }  
+while (attemp < 6) {
+    let userAttemp = Number(prompt('Please guess one of my favorite numbers between 1 and 20 , you have :'+ chance+' chances'));
+    for (let i = 0; i < answers.length; i++) {
+        if (userAttemp == answers[i]) {
+            alert('Correct! :)');
+            attemp=7;
+            score++;
+            break;
+
+        }
+
+    }
+    if (attemp<6){
+    
+    alert('Not correct!');
 }
-
-    userAttemp=prompt('Sorry, Try another number :(');
-    attemp++;
+chance--;
+attemp++;
 }
 
 //console.log(answers);
 
-alert('My answers are: \n I studied Computer Science in Yarmouk University. \n I live in Amman. \n Coffee is my favourite drink. \n I\'m Not a Morning person. \n I love Cat So Much.\n my favorite number is 7 \n The numbers are: 3,6,8,12,15,19. \n YOUR SCORE IS: '+ score);
-alert('THANK YOU '+ userName.toUpperCase()+' ,I HOPE YOU KNOW ME BETTER NOW :)');
+alert('My answers are: \n I studied Computer Science in Yarmouk University. \n I live in Amman. \n Coffee is my favourite drink. \n I\'m Not a Morning person. \n I love Cat So Much.\n my favorite number is 7 \n The numbers are: 3,6,8,12,15,19. \n YOUR SCORE IS: ' + score);
+alert('THANK YOU ' + userName.toUpperCase() + ' ,I HOPE YOU KNOW ME BETTER NOW :)');
